f0e288a42527b2e80f3006babf2f6a02116cc8bf
im-perry <61919070+im-perry@users.noreply.github.com>
2020-12-06 21:24:37 +0200

    Initial commit

---
+++ public_html/dealers.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!doctype html>
+<html>
+    <head>
+        <title>Ford Dealers</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css" type="text/css">
+        <link rel="stylesheet" href="css/myCss.css" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        <script type = "text/javascript" src="js/bootstrap.min.js"></script>
+        <script type = "text/javascript" src="js/script.js"></script>
+        
+    </head>
+    <body>
+        <nav class = "navbar bg-primary">
+            <div class="container" style="padding-bottom: 10px;">
+                <a href="index.php"><img src="https://fontmeme.com/permalink/201206/ccef735378e68e17ba5812191ac8f2ff.png" alt="ford-font" border="0" style="float: left; padding-top: 14px; margin-right: 10px;"></a>
+                <ul class="nav navbar-nav text-info">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li class="active"><a href="">About</a></li>
+                    <li class="active"><a href="support.php">Support</a></li>
+                </ul>
+                <ul class="nav navbar-nav text-info" style="float: right;">
+                    <li class="active"><a href="dealers.php">Dealers</a></li>
+                </ul>
+            </div>   
+        </nav>
+        <div class="container">
+            <div class="h1">Ford Dealer in Craiova</div>
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?key=AIzaSyCsMMgwhEdPKJF-oQZoxJIixrV2jELy8EE&pb=!1m18!1m12!1m3!1d2855.8037199595565!2d23.843695815883187!3d44.29342527910464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4752d6662057fa47%3A0xb581310937f756cc!2sFord%20Rom%C3%A2nia!5e0!3m2!1sro!2sro!4v1607271669006!5m2!1sro!2sro" 
+                        width="800" height="600" 
+                        frameborder="1" 
+                        allowfullscreen="" aria-hidden="false" tabindex="0">
+                </iframe>
+            </div>
+            <div class="details">
+                <p>Email: fordcv@ford.com
+                    <br>
+                    Telephone: 0372516255
+                </p>
+            </div>
+        </div>
+        <footer class="page-footer font-small pers navbar-fixed-bottom">
+            <div class="container">
+                <ul class="list-unstyled list-inline text-center">
+                    <li class="list-inline">
+                        <a href="https://www.facebook.com/mihaicezar.perisoara" class="fa fa-facebook"></a>
+                    </li>
+                    <li class="list-inline">
+                        <a href="https://www.instagram.com/mihai.per/" class="fa fa-instagram"></a>
+                    </li>
+                    <li class="list-inline">
+                        <a href="https://www.reddit.com/user/198Perry" class="fa fa-reddit"></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="text-center py-3">
+                Perisoara Mihai &copy;
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ public_html/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <title>Ford</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css" type="text/css">
+        <link rel="stylesheet" href="css/myCss.css" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        <script type = "text/javascript" src="js/bootstrap.min.js"></script>
+        <script type = "text/javascript" src="js/script.js"></script>
+    </head>
+    <body>
+        <nav class = "navbar bg-primary">
+            <div class="container" style="padding-bottom: 10px;">
+                <a href="index.php"><img src="https://fontmeme.com/permalink/201206/ccef735378e68e17ba5812191ac8f2ff.png" alt="ford-font" border="0" style="float: left; padding-top: 14px; margin-right: 10px;"></a>
+                <ul class="nav navbar-nav text-info">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li class="active"><a href="">About</a></li>
+                    <li class="active"><a href="support.php">Support</a></li>
+                </ul>
+                <ul class="nav navbar-nav text-info" style="float: right;">
+                    <li class="active"><a href="dealers.php">Dealers</a></li>
+                </ul>
+            </div>   
+        </nav>
+        <div class="container">
+            <div class="text-center h1">
+                Ford Motor Company
+            </div>
+            <div class="text-center h3">
+                Go Further
+            </div>
+        </div>
+        <footer class="page-footer font-small pers navbar-fixed-bottom">
+            <div class="container">
+                <ul class="list-unstyled list-inline text-center">
+                    <li class="list-inline">
+                        <a href="https://www.facebook.com/mihaicezar.perisoara" class="fa fa-facebook"></a>
+                    </li>
+                    <li class="list-inline">
+                        <a href="https://www.instagram.com/mihai.per/" class="fa fa-instagram"></a>
+                    </li>
+                    <li class="list-inline">
+                        <a href="https://www.reddit.com/user/198Perry" class="fa fa-reddit"></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="text-center py-3">
+                Perisoara Mihai &copy;
+            </div>
+        </footer>
+    </body>
+</html>
